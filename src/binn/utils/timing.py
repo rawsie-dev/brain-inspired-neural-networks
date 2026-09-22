476931ccb3054c21f523c5fc37f5f38403a679ca
Rawsie <rawsienguyen@gmail.com>
2026-09-23 00:29:27 +0700

    random seeding and timing

---
+++ src/binn/utils/timing.py
@@ -0,0 +1,10 @@
+from time import perf_counter
+
+
+class Timer:
+    def __enter__(self): 
+        self.start = perf_counter()
+        return self
+    
+    def __exit__(self, *_): 
+        self.seconds = perf_counter() - self.start
